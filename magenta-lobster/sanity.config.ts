a65cd0927d7d222e6a59c12d6924ecb79af4f2e1
roysaucedo <roy.a.saucedo@gmail.com>
2023-03-15 12:18:02 -0700

    init

---
+++ magenta-lobster/sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'magenta-lobster',
+
+  projectId: 't8e1uf3z',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
